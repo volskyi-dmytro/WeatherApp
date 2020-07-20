d982de4f033ea08625a32f5ab26712b6fa1aca38
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-07-20 15:36:10 +0300

    Initial commit

---
+++ src/test/java/com/stpunk47/WeatherApp/WeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stpunk47.WeatherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
